2e2c46d93a5d3922569a6227666d6593acd7e064
dargud <gudymamain@gmail.com>
2023-11-27 20:43:49 +0000

    URL-update

--- e2e/pd/tests/e2e/oldApp.spec.js
+++ e2e/pd/tests/e2e/oldApp.spec.js
@@ -14,8 +14,8 @@ test.describe('Smoke test suite, without logged in user', () => {
         let onLearnPage = new LearnPage (page);
 
         //Prepare the page for tests:
-        await page.goto(process.env.URL);
-        await expect(page).toHaveURL(process.env.URL);
+        await page.goto(process.env.URLPROD);
+        await expect(page).toHaveURL(process.env.URLPROD);
         await onLearnPage.clickDismissButton();
         await onLearnPage.isMessageDisplayed('Welcome to Unity Learn');
     });
