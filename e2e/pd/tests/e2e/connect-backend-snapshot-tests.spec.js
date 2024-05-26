9df2a73e08f16eb86a7e44fe1c29bc784e4ca56c
dargud <39130466+dargud@users.noreply.github.com>
2024-05-26 19:54:18 +0100

    actions-test

---
+++ e2e/pd/tests/e2e/connect-backend-snapshot-tests.spec.js
@@ -0,0 +1,33 @@
+import { test, expect } from "@playwright/test";
+import { LearnPage } from "../../page-objects/connect-backend/LearnPage.js";
+import { TutorialWidget } from "../../Widgets/TutorialWidget.js";
+import { LoginPage } from "../../page-objects/LoginPage.js";
+import { HeaderOfAnyPage } from "../../page-objects/connect-backend/HeaderOfAnyPage.js";
+
+const baseUrl = "https://learn.unity.com";
+const prefix = process.env.PREFIX;
+
+test.describe("Connected BE (not logined user): Snapshot tests", () => {
+  test("Connected BE: Home page", async ({ page }) => {
+    await page.goto(baseUrl);
+    await page.waitForTimeout(1000 * 1);
+    await expect(await page.screenshot()).toMatchSnapshot(
+      `${prefix}-home-page.png`,
+      {
+        maxDiffPixels: 100000,
+      }
+    );
+  });
+
+  test("Connected BE: Pathways page", async ({ page }) => {
+    await page.goto(`${baseUrl}/pathways`);
+    await page.waitForLoadState();
+    await expect(await page.screenshot()).toMatchSnapshot(
+      `${prefix}-pathways-page.png`,
+      {
+        maxDiffPixels: 100000,
+      }
+    );
+  });
+
+});
