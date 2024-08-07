fc4bbbe5a517b0e937616dfd9ef086f9744a462d
dargud <39130466+dargud@users.noreply.github.com>
2024-08-07 12:19:09 +0100

    update

--- e2e/pd/tests/e2e/connect-backend-accessibility-tests.spec.js
+++ e2e/pd/tests/e2e/connect-backend-accessibility-tests.spec.js
@@ -268,7 +268,7 @@ test.describe("Connected BE (not logined user): Accessibility tests", () => {
   });
 });
 
-test.describe.only("Connected BE (logined user): Accessibility tests", () => {
+test.describe("Connected BE (logined user): Accessibility tests", () => {
   test.beforeEach(async ({ page }) => {
     test.slow(); // Delete after page performance improvements
     const app = new PageManager(page);
