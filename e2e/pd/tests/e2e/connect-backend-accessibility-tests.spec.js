c4dd410e419fab47a968084a8d4e90eafaf4a478
dargud <39130466+dargud@users.noreply.github.com>
2024-08-07 11:32:35 +0100

    update

--- e2e/pd/tests/e2e/connect-backend-accessibility-tests.spec.js
+++ e2e/pd/tests/e2e/connect-backend-accessibility-tests.spec.js
@@ -12,7 +12,7 @@ const user = {
 const tags = ["wcag22a", "wcag22aa"];
 
 test.describe("Connected BE (not logined user): Accessibility tests", () => {
-  test("Connected BE: Home page", async ({ page }, testInfo) => {
+  test.only("Connected BE: Home page", async ({ page }, testInfo) => {
     await page.goto(`${BASE_URL}`);
     // await page.waitForLoadState("domcontentloaded");
     const accessibilityScanResults = await new AxeBuilder({ page })
