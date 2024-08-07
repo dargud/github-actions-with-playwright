5406fbd29478b39186af05e3e42722e4269ac2c8
dargud <39130466+dargud@users.noreply.github.com>
2024-08-07 11:56:03 +0100

    update

--- e2e/pd/tests/e2e/connect-backend-accessibility-tests.spec.js
+++ e2e/pd/tests/e2e/connect-backend-accessibility-tests.spec.js
@@ -268,7 +268,7 @@ test.describe("Connected BE (not logined user): Accessibility tests", () => {
   });
 });
 
-test.describe.only("Connected BE (logined user): Accessibility tests", () => {
+test.describe("Connected BE (logined user): Accessibility tests", () => {
   test.beforeEach(async ({ page }) => {
     test.slow(); // Delete after page performance improvements
     const app = new PageManager(page);
@@ -290,7 +290,7 @@ test.describe.only("Connected BE (logined user): Accessibility tests", () => {
     }
   });
 
-  test("Connected BE: Home page", async ({ page }, testInfo) => {
+  test.only("Connected BE: Home page", async ({ page }, testInfo) => {
     await page.goto(`${BASE_URL}`);
     // await page.waitForLoadState("domcontentloaded");
     const accessibilityScanResults = await new AxeBuilder({ page })
