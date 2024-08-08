d5fd420a90e8560a22863bb183eb8da3d144ebc2
dargud <39130466+dargud@users.noreply.github.com>
2024-08-08 11:45:20 +0100

    update

--- e2e/pd/config.js
+++ e2e/pd/config.js
@@ -4,7 +4,7 @@ config();
 
 const runtimeConfig = {
   BASE_URL: process.env.BASE_URL || "error",
-  ENV: process.env.PREFIX || "error",
+  ENV: process.env.ENV || "error",
   USER_EMAIL: process.env.TEST_USER_EMAIL || "error",
   USER_PASS: process.env.TEST_USER_PASS || "error",
   GMAIL_TESTER_CREDENTIALS: process.env.GMAIL_TESTER_CREDENTIALS || "error",
